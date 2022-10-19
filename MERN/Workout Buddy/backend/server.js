4fe3993b1127faaa9424d912c45e1833d39678b8
anupamb-2000 <anupam.b@inapp.com>
2022-10-19 22:42:37 +0530

    MERN - Workout Buddy : Backend Complete

--- MERN/Workout Buddy/backend/server.js
+++ MERN/Workout Buddy/backend/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app= express()
+
+// middleware
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -11,11 +16,16 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({msg: "Welcome to the app"})
-})
+app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000')
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+ .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('connected to db and listening on port', process.env.PORT)
+    })
+ })
+ .catch((err) => {
+    console.error(err)
+ })
