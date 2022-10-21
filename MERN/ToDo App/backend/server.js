8dd7deaefe9ba6add9899cb204fbe6ab68d499a9
Anupam <kidab2k@gmail.com>
2022-10-21 17:34:38 +0530

    MERN : ToDo App - Initial Commit!

---
+++ MERN/ToDo App/backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require('express')
+
+// express app
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({ mssg: "Welcome to the application"})
+})
+
+// listen
+app.listen(process.env.PORT, () => {
+    console.log('listening on port 4000!')
+})
